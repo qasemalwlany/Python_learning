c9e3bcfc9df7c2cae813dbb5a273e50955a54050
qasemalwlany <qasemalwlany@gmail.com>
2026-04-05 05:59:23 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+NAME="Qasem"
+JOP="It"
+Hobby="Games and Programming"
+A="C:\new_folder\test"
+print(r"C:\new_folder\test")
+print('He said "Python is Amazing"')
+print(f"""welcome I'm {NAME}
+and my jop is {JOP}
+and my Hobby is {Hobby}""")
